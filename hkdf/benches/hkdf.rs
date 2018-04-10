fe9cf2110c09cf334d63cbd046f5fca989b951cf
Brian Warner <warner@lothar.com>
2018-04-10 01:42:19 -0700

    benchmarks: test both new expand() and deprecated derive()

--- hkdf/benches/hkdf.rs
+++ hkdf/benches/hkdf.rs
@@ -7,25 +7,50 @@ use bencher::Bencher;
 use hkdf::Hkdf;
 use sha2::Sha256;
 
-fn sha256_10(b: &mut Bencher) {
+fn sha256_10_derive(b: &mut Bencher) {
     b.iter(|| Hkdf::<Sha256>::new(&[], &[]).derive(&[], 10));
     b.bytes = 10u64;
 }
 
-fn sha256_1k(b: &mut Bencher) {
+fn sha256_1k_derive(b: &mut Bencher) {
     b.iter(|| Hkdf::<Sha256>::new(&[], &[]).derive(&[], 1024));
     b.bytes = 1024u64;
 }
 
-fn sha256_8k(b: &mut Bencher) {
+fn sha256_8k_derive(b: &mut Bencher) {
     b.iter(|| Hkdf::<Sha256>::new(&[], &[]).derive(&[], 8000));
+    b.bytes = 8000u64;
+}
+
+fn sha256_10(b: &mut Bencher) {
+    let mut out = Vec::new();
+    out.resize(10, 0u8);
+    b.iter(|| Hkdf::<Sha256>::new(&[], &[]).expand(&[], &mut out));
+    b.bytes = 10u64;
+}
+
+fn sha256_1k(b: &mut Bencher) {
+    let mut out = Vec::new();
+    out.resize(1024, 0u8);
+    b.iter(|| Hkdf::<Sha256>::new(&[], &[]).expand(&[], &mut out));
+    b.bytes = 1024u64;
+}
+
+fn sha256_8k(b: &mut Bencher) {
+    let mut out = Vec::new();
+    out.resize(8000, 0u8);
+    b.iter(|| Hkdf::<Sha256>::new(&[], &[]).expand(&[], &mut out));
     b.bytes = 8000u64;
 }
 
 // note: SHA-256 output limit is 255*32=8160 bytes
 
 benchmark_group!(benches,
+                 sha256_10_derive,
+                 sha256_1k_derive,
+                 sha256_8k_derive,
                  sha256_10,
                  sha256_1k,
-                 sha256_8k);
+                 sha256_8k
+);
 benchmark_main!(benches);
